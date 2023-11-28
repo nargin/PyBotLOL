65e23183c3f2167d95efe56c42edfdee72c49859
nargin <mejumine@gmail.com>
2023-11-28 18:55:52 +0100

    new features will continue later

--- league_related.py
+++ league_related.py
@@ -2,27 +2,135 @@ import discord
 import json
 from riotwatcher import LolWatcher, ApiError
 
-def mastery_champion(summoner_name, lol_watcher):
+def profile_ranked(data, lol_watcher): # command !pr
 	try:
-		user = lol_watcher.summoner.by_name('euw1', summoner_name)
+		user = lol_watcher.summoner.by_name(data.regions, data.nickname)
 	except ApiError as err:
-		return discord.Embed(title='User not found', description=f'User {summoner_name} not found', color=0xff0000, type='rich')
+		return discord.Embed(title='User not found', description=f'User {data.nickname} not found', color=0xff0000, type='rich')
 
 	user_id = user['id']
 
 	try:
-		masteries = lol_watcher.champion_mastery.by_summoner('euw1', user_id)
+		ranked_stats = lol_watcher.league.by_summoner(data.regions, user_id)
 	except ApiError as err:
-		return discord.Embed(title='User not found', description=f'User {summoner_name} not found', color=0xff0000, type='rich')
+		return discord.Embed(title='User not found', description=f'User {data.nickname} not found', color=0xff0000, type='rich')
+
+	soloq_stats = None
+	for queue in ranked_stats:
+		if queue['queueType'] == 'RANKED_SOLO_5x5':
+			soloq_stats = queue
+			break
+
+	if soloq_stats == None:
+		return discord.Embed(title='User not found', description=f'User {data.nickname} not found', color=0xff0000, type='rich')
+
+	tier = soloq_stats['tier']
+	rank = soloq_stats['rank']
+	lp = soloq_stats['leaguePoints']
+	wins = soloq_stats['wins']
+	losses = soloq_stats['losses']
+	winrate = wins / (wins + losses) * 100
+
+	match tier:
+		case 'IRON':
+			color = 0x434343
+		case 'BRONZE':
+			color = 0x8B4513
+		case 'SILVER':
+			color = 0xC0C0C0
+		case 'GOLD':
+			color = 0xFFD700
+		case 'PLATINUM':
+			color = 0x00FFFF
+		case 'EMERALD':
+			color = 0x00FF00
+		case 'DIAMOND':
+			color = 0x00BFFF
+		case 'MASTER':
+			color = 0xFF00FF
+		case 'GRANDMASTER':
+			color = 0xFF0000
+		case 'CHALLENGER':
+			color = 0xFFA500
+		case _:
+			color = 0x000000
+
+	message = f'{tier} {rank} {lp} LP\n'
+	if data.option == '1':
+		message += f'Wins : {wins} - Losses : {losses} - Winrate : {winrate:.2f}% '
+		message += ':warning:' if winrate < 50 else ':white_check_mark:'
+	
+	embed = discord.Embed(title=f'{data.nickname}\'s soloq rank', description=f'**{message}**', color=color, type='rich')
+	embed.set_thumbnail(url=f'https://raw.communitydragon.org/latest/game/assets/ux/tftmobile/particles/tft_regalia_{tier.lower()}.png')
+	return embed
+
+
+
+
+def	winrate_soloq(data, lol_watcher):
+	try:
+		user = lol_watcher.summoner.by_name(data.regions, data.nickname)
+	except ApiError as err:
+		return discord.Embed(title='User not found', description=f'User {data.nickname} not found', color=0xff0000, type='rich')
+
+	user_id = user['id']
+
+	try:
+		ranked_stats = lol_watcher.league.by_summoner(data.regions, user_id)
+	except ApiError as err:
+		return discord.Embed(title='No ranked stats found', description=f'User {data.nickname} has no ranked stats', color=0xff0000, type='rich')
+
+	soloq_stats = None
+	for queue in ranked_stats:
+		if queue['queueType'] == 'RANKED_SOLO_5x5':
+			soloq_stats = queue
+			break
+
+	if soloq_stats == None:
+		return discord.Embed(title='No ranked stats found', description=f'User {data.nickname} has no ranked stats', color=0xff0000, type='rich')
+
+	wins = soloq_stats['wins']
+	losses = soloq_stats['losses']
+	winrate = wins / (wins + losses) * 100
+
+	message = f'Wins : {wins} - Losses : {losses} - Winrate : {winrate:.2f}% '
+	message += ':warning:' if winrate < 50 else ':white_check_mark:'
+
+	color = 0xff0000 if winrate < 50 else 0x00ff00
+	embed = discord.Embed(title=f'{data.nickname}\'s soloq winrate', description=message, color=color, type='rich')
+	
+	return embed
+
+
+
+
+def mastery_champion(data, lol_watcher):
+	
+	data.option = 3 if data.option == 0 else 10 if data.option > 10 else data.option # 3 at min and 25 at max
+
+	try:
+		user = lol_watcher.summoner.by_name(data.regions, data.nickname)
+	except ApiError as err:
+		return discord.Embed(title='User not found', description=f'User {data.nickname} not found', color=0xff0000, type='rich')
+
+	user_id = user['id']
+
+	try:
+		masteries = lol_watcher.champion_mastery.by_summoner(data.regions, user_id)
+	except ApiError as err:
+		return discord.Embed(title='User not found', description=f'User {data.nickname} not found', color=0xff0000, type='rich')
 
 	masteries = sorted(masteries, key=lambda k: k['championPoints'], reverse=True)
 
-	versions = lol_watcher.data_dragon.versions_for_region('euw1')
+	versions = lol_watcher.data_dragon.versions_for_region(data.regions)
 	current_version = versions['n']['champion']
 
-	embed = discord.Embed(title=f'{summoner_name}\'s mastery', description='', color=0x00ff00, type='rich')
+	color_bg = 0xFFB233
+	embed = discord.Embed(title=f'{data.nickname}\'s mastery', description='', color=color_bg, type='rich')
 
-	for i in range(3):
+	num_champs = len(lol_watcher.data_dragon.champions(current_version)['data'])
+	# for all champs
+	for i in range(data.option):
 
 		champion_id = masteries[i]['championId']
 		champion_level = masteries[i]['championLevel']
@@ -40,22 +148,47 @@ def mastery_champion(summoner_name, lol_watcher):
 		champion_icon = lol_watcher.data_dragon.champions(current_version)['data'][champion_key]['image']['full']
 		champion_icon_url = f'http://ddragon.leagueoflegends.com/cdn/{current_version}/img/champion/{champion_icon}'
 		
-		embed.add_field(name=f'{champion_name}', value=f'Level {champion_level} - {champion_points} points', inline=False)
+		match champion_level:
+			case 7:
+				mastery_icon = '<:mastery_7:1179010217963438172>'
+			case 6:
+				mastery_icon = '<:mastery_6:1179010170127388772>'
+			case 5:
+				mastery_icon = '<:mastery_5:1179010136505856011>'
+			case 4:
+				mastery_icon = '<:mastery_4:1179010098610315264>'
+			case 3:
+				mastery_icon = '<:mastery_3:1179010071779344437>'
+			case 2:
+				mastery_icon = '<:mastery_2:1179010025096761416>'
+			case 1:
+				mastery_icon = '<:mastery_1:1179009953231556638>'
+			case _:
+				mastery_icon = ':zero:'
+
+		embed.add_field(name=f'{champion_name}', value=f'Level {mastery_icon} - {champion_points}', inline=False)
 		if i == 0:
+			if champion_level == 7:
+				color_bg = 0x3396FF
+			elif champion_level == 6:
+				color_bg = 0xFF33E9
+			elif champion_level == 5:
+				color_bg = 0xC42D0B
 			embed.set_thumbnail(url=champion_icon_url)
 
+	embed.color = color_bg
 	return embed
 
 
 
 
-def profile_user(summoner_name, lol_watcher):
-	version = lol_watcher.data_dragon.versions_for_region('euw1')['n']['champion']
+def profile_user(data, lol_watcher):
+	version = lol_watcher.data_dragon.versions_for_region(data.regions)['n']['champion']
 
 	try:
-		user = lol_watcher.summoner.by_name('euw1', summoner_name)
+		user = lol_watcher.summoner.by_name(data.regions, data.nickname)
 	except ApiError as err:
-		return discord.Embed(title='User not found', description=f'User {summoner_name} not found', color=0xff0000, type='rich')
+		return discord.Embed(title='User not found', description=f'User {data.nickname} not found', color=0xff0000, type='rich')
 
 	nick = user['name']
 	level = user['summonerLevel']
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-import os
+import os, sys
 import discord
 from riotwatcher import LolWatcher, ApiError
 from dotenv import load_dotenv
-from league_related import mastery_champion, profile_user
+from league_related import mastery_champion, profile_ranked, winrate_soloq
 from personal import personal
+from helper import helper, Command, command_list, morpion
 
 test = False
 
+
+# Load environment variables
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 RIOT_API_KEY = os.getenv('LOL_TOKEN')
+region = os.getenv('LOL_REGION')
 
+# Initialize RiotWatcher
 lol_watcher = LolWatcher(RIOT_API_KEY)
 
-
+# Discord client
 intents = discord.Intents.all()
 intents.members = True
 
@@ -32,32 +37,61 @@ async def on_message(message):
 		return
 
 	input_message = message.content.lower()
-	input_message = input_message.split(' ', 1)
 
-	print(f'#{message.channel.name} {message.author} : {message.content}')
+	if input_message.startswith('!') == False:
+		return
 
-	if input_message[0] == '!big':
-		await message.channel.send("https://tenor.com/view/big-snoopa-gif-18474914")
+	split = input_message.split(' ')
+	data_command = Command(split, region, lol_watcher)
+
+	# print(f'command : {data_command.command}, option : {data_command.option}, summoner_name : {data_command.nickname}')
+
+	match data_command.command:
+		case None :
+			pass
+
+		case 'big':
+			await message.channel.send("https://tenor.com/view/big-snoopa-gif-18474914")
+			return
+		
+		case 'profile': # Summoner Profile
+			await message.channel.send(embed=profile_ranked(data_command, lol_watcher))
+			return
+
+		case 'mastery': # Mastery
+			await message.channel.send(embed=mastery_champion(data_command, lol_watcher))
+			return
+		
+		case 'winrate':
+			await message.channel.send(embed=winrate_soloq(data_command, lol_watcher))
+			return
+
+		case 'help':
+			await message.channel.send(embed=helper())
+			return
+		
+		case 'morpion':
+			await message.channel.send(morpion())
+			return
+
+		case _:
+			pass
+
+	command_personal = personal(input_message)
+
+	if command_personal != None :
+		await message.channel.send(command_personal)
+
+	else :
+		print(f'#{message.channel.name} {message.author} : {message.content} - Command not found')
 	
-	if input_message[0] == '!s':
-		await message.channel.send(embed=profile_user(input_message[1], lol_watcher))
-
-	if input_message[0] == '!m':
-		await message.channel.send(embed=mastery_champion(input_message[1], lol_watcher))
-
-	
-	# command_personal = personal(input_message)
-
-	# if command_personal != None and type(command_personal) == str:
-	# 	await message.channel.send(command_personal)
-	# elif command_personal != None and type(command_personal) == list:
-	# 	await message.channel.send(command_personal[0])
-	# 	await message.channel.send(command_personal[1])
-	
-	if input_message[0] == '!help':
-		await message.channel.send("**Commands : \n\t!big\n\t!s <summoner_name>\n\t!m <summoner_name>**")
 	
 
-test = False
+# TO DO :
+# Gain lp command
+# death of 20 last games
+# rank and lp soloq command
+
+test = 1 if len(sys.argv) > 1 and sys.argv[1] == 'test' else 0
 client.run(DISCORD_TOKEN)
 
